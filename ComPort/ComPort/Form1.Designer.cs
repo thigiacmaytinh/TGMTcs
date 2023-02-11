f685ecfdaef4d57ffccf84520377392e6ea71843
vohungvi <vohungvi27@gmail.com>
2023-02-11 22:42:01 +0700

    import comport

---
+++ ComPort/ComPort/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ComPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách cổng";
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(116, 30);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(148, 21);
+            this.cbPorts.TabIndex = 1;
+            this.cbPorts.SelectedIndexChanged += new System.EventHandler(this.cbPorts_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(480, 141);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dữ liệu nhận được ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Gửi dữ liệu";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Enabled = false;
+            this.txtSend.Location = new System.Drawing.Point(76, 309);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(247, 20);
+            this.txtSend.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(345, 306);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Gửi";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 353);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Giao tiếp cổng COM";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ComPort/ComPort/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ComPort
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort _serialPort = new SerialPort();
+        Thread readThread;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Giá trị mặc định
+            _serialPort.BaudRate = 9600;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+
+            readThread = new Thread(Read);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        // hiện danh sách các cổng trong máy
+        void SetPortName(string portName)
+        {
+
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+            {
+                MessageBox.Show("Không đúng tên cổng");
+            }
+
+            _serialPort.Close();
+            _serialPort.PortName = portName;            
+            _serialPort.Open();
+
+            MessageBox.Show("Đã chọn cổng " + portName);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        // hiện danh sách port lên combobox
+        void ShowPortList()
+        {
+            string[] portList = SerialPort.GetPortNames();
+            if (portList.Length == 0)
+            {
+                cbPorts.Text = "Không tìm thấy cổng";
+                cbPorts.Enabled = false;
+                return;
+            }
+
+            foreach (string s in portList)
+            {
+                cbPorts.Items.Add(s);
+            }            
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowPortList();
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void cbPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetPortName(cbPorts.SelectedItem.ToString());
+            textBox1.Enabled = true;
+            txtSend.Enabled = true;
+            btnSend.Enabled = true;
+
+            readThread.Start();
+            readThread.Abort();
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        void Read()
+        {
+            while (true)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    textBox1.Text += message;
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        //gửi dữ liệu qua cổng COM
+        void Write(string text)
+        {
+            if (text == "")
+                return;
+
+            _serialPort.WriteLine(text);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _serialPort.Close();
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtSend.Text == "")
+            {
+                MessageBox.Show("Text rỗng");
+                return;
+            }
+
+            Write(txtSend.Text);
+            txtSend.Clear();
+            readThread.Abort();
+        }
+    }
+}
