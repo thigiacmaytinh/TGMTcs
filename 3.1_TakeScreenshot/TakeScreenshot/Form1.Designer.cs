a58955ba49d3cedc960ac8836098fbad76abd98e
vohungvi <vohungvi@vohungvi.com>
2024-08-14 14:42:47 +0700

    + add example take screenshot

---
+++ 3.1_TakeScreenshot/TakeScreenshot/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TakeScreenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_takeScreenshot = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_takeScreenshot
+            // 
+            this.btn_takeScreenshot.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_takeScreenshot.Location = new System.Drawing.Point(100, 12);
+            this.btn_takeScreenshot.Name = "btn_takeScreenshot";
+            this.btn_takeScreenshot.Size = new System.Drawing.Size(139, 38);
+            this.btn_takeScreenshot.TabIndex = 0;
+            this.btn_takeScreenshot.Text = "Take screenshot";
+            this.btn_takeScreenshot.UseVisualStyleBackColor = true;
+            this.btn_takeScreenshot.Click += new System.EventHandler(this.btn_takeScreenshot_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(937, 479);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 547);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_takeScreenshot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_takeScreenshot;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 3.1_TakeScreenshot/TakeScreenshot/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakeScreenshot
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetDesktopWindow();
+        [DllImport("User32.dll")]
+        public static extern int GetWindowDC(int hWnd);
+        [DllImport("User32.dll")]
+        public static extern int ReleaseDC(int hWnd, int hDC);
+
+        [DllImport("GDI32.dll")]
+        public static extern bool BitBlt(int hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, int hdcSrc, int nXSrc, int nYSrc, int dwRop);
+        [DllImport("GDI32.dll")]
+        public static extern int CreateCompatibleBitmap(int hdc, int nWidth, int nHeight);
+        [DllImport("GDI32.dll")]
+        public static extern int CreateCompatibleDC(int hdc);
+        [DllImport("GDI32.dll")]
+        public static extern bool DeleteDC(int hdc);
+        [DllImport("GDI32.dll")]
+        public static extern bool DeleteObject(int hObject);
+        [DllImport("GDI32.dll")]
+        public static extern int GetDeviceCaps(int hdc, int nIndex);
+        [DllImport("GDI32.dll")]
+        public static extern int SelectObject(int hdc, int hgdiobj);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_takeScreenshot_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = TakeScreenshot();
+            pictureBox1.Image = bitmap;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static Bitmap TakeScreenshot()
+        {
+            int hdcSrc = GetWindowDC(GetDesktopWindow()), // Get a handle to the desktop window
+            hdcDest = CreateCompatibleDC(hdcSrc), // Create a memory device context
+            hBitmap = CreateCompatibleBitmap(hdcSrc, // Create a bitmap and place it in the memory DC
+            GetDeviceCaps(hdcSrc, 8), GetDeviceCaps(hdcSrc, 10));
+            // GDI32.GetDeviceCaps(hdcSrc,8) returns the width of the desktop window
+            // GDI32.GetDeviceCaps(hdcSrc,10) returns the height of the desktop window
+            SelectObject(hdcDest, hBitmap); // Required to create a color bitmap
+            BitBlt(hdcDest, 0, 0, GetDeviceCaps(hdcSrc, 8), // Copy the on-screen image into the memory DC
+            GetDeviceCaps(hdcSrc, 10), hdcSrc, 0, 0, 0x00CC0020);
+            Bitmap image = new Bitmap(Image.FromHbitmap(new IntPtr(hBitmap)),
+                Image.FromHbitmap(new IntPtr(hBitmap)).Width,
+                Image.FromHbitmap(new IntPtr(hBitmap)).Height);
+
+            //SaveImageAs(hBitmap, fileName, imageFormat); // Save the screen-capture to the specified file using the designated image format
+            // Release the device context resources back to the system
+            ReleaseDC(GetDesktopWindow(), hdcSrc);
+            DeleteDC(hdcDest);
+            DeleteObject(hBitmap);
+
+
+            return image;
+        }
+    }
+}
