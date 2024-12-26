0ceaa5e15b91413f9db849dd4bdf132b1a2f926c
vohungvi <vohungvi@vohungvi.com>
2024-12-26 15:13:45 +0700

    + add project 6.2

---
+++ 6.2_InternetChecker/InternetChecker/FormMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace InternetChecker
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.phóngToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(66)))), ((int)(((byte)(139)))));
+            this.label1.Location = new System.Drawing.Point(274, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trạng thái:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InternetChecker.Properties.Resources.check_error;
+            this.pictureBox1.Location = new System.Drawing.Point(527, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Kiểm tra internet";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.phóngToToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 48);
+            // 
+            // phóngToToolStripMenuItem
+            // 
+            this.phóngToToolStripMenuItem.Name = "phóngToToolStripMenuItem";
+            this.phóngToToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.phóngToToolStripMenuItem.Text = "Phóng to";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.exitToolStripMenuItem.Text = "Thoát chương trình";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(48, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1261, 423);
+            this.panel1.TabIndex = 9;
+            this.panel1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(30, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1208, 62);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Bạn bắt buộc phải báo quản lý xử lý trước khi máy hư";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(138, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(972, 62);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Máy tính này đang có vấn đề về phần cứng";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(240)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(1364, 711);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Kiểm tra internet";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem phóngToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 6.2_InternetChecker/InternetChecker/FormMain.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.ServiceProcess;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TGMTcs;
+using System.Net.Sockets;
+using System.Globalization;
+using System.Timers;
+using System.Net.NetworkInformation;
+
+namespace InternetChecker
+{
+    public partial class FormMain : Form
+    {
+        
+        static FormMain m_instance;
+
+        string server = "https://sieuthi.phuoctrung.com";
+        string userReceive = "anhvietlienket@gmail.com";
+        string secretKey = "OXXBIRALRTDAWGIIIKDQFTAYECVBSANDGVHIQIDO";
+
+
+        System.Timers.Timer timerChecktime = new System.Timers.Timer();
+
+
+        bool m_formClosed = false;
+        bool m_soundPlayed = false;
+        bool m_isUserClose = false;
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + TGMTutil.GetVersion();
+#if DEBUG
+            server = "http://localhost";
+            userReceive = "vohungvi27@gmail.com";
+
+            this.Text += " (Debug)";
+#endif
+
+            timerChecktime.Elapsed += TimerChecktime_Elapsed;
+            timerChecktime.Interval = 5 * 1000; //5 seconds
+            timerChecktime.Enabled = true;
+
+
+            CheckInternetTimeAsync();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (m_isUserClose)
+                return;
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static FormMain GetInstance()
+        {
+            if (m_instance == null)
+                m_instance = new FormMain();
+            return m_instance;
+        }       
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void UpdateFormText(string windowText)
+        {
+            if(windowText == "THONG BAO" || windowText == "Page Setup")
+            {
+                SendEmailDialogShown(windowText);                
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void SendEmailDialogShown(string windowText)
+        {
+            try
+            {
+                Bitmap bmp = TGMTwindows.TakeScreenshot();
+                bmp.Save(DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".jpg");
+
+
+                string api = "/api/email/send";
+                var values = new Dictionary<string, string>
+                {
+                    {"title", "Phần mềm POS hiện popup tại máy " + Environment.MachineName},
+                    {"userReceive", userReceive},
+                    {"purpose", "DialogShown"},
+                    {"body", "Chào bạn. Phần mềm POS đã hiện popup " + windowText + " vào lúc " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")},
+                    {"imageBase64", TGMTimage.ImageToBase64(bmp) },
+                    {"secretKey", secretKey}
+                };
+                TGMTonline.SendPOSTrequest(server + api, values, OnResponse);
+
+                TGMTsound.PlaySoundAsync("ting-2.mp3");
+            }
+            catch (Exception ex)
+            {
+                WriteErrorLog(ex.Message);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void OnResponse(int statusCode, string response)
+        {
+            //do nothing
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static void WriteErrorLog(string ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("TimeChecker.exe"))
+            {
+                MessageBox.Show("Không tìm thấy file TimeChecker.exe");
+                return;
+
+            }
+            string cmd = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe";
+            cmd += " /K" + Application.StartupPath + "\\TimeChecker.exe";
+
+            Process.Start("cmd.exe", cmd);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void SendEmailProgramStoped()
+        {
+            if (File.Exists(@"D:\debug.txt"))
+                return;
+
+            try
+            {
+                string api = "/api/email/send";
+                var values = new Dictionary<string, string>
+                {
+                    {"title", "Phần mềm kiểm tra ngày tháng " +  Environment.MachineName + " đã dừng"},
+                    {"userReceive", userReceive},
+                    {"purpose", "WrongDatetime"},
+                    {"body", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")},
+                    {"secretKey", secretKey}
+                };
+                TGMTonline.SendPOSTrequest(server + api, values, OnResponse);
+
+                Thread.Sleep(3000);
+            }
+            catch (Exception ex)
+            {
+                WriteErrorLog(ex.Message);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void TimerChecktime_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timerChecktime.Stop();
+            CheckInternetTime();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void CheckInternetTimeAsync()
+        {
+            var t = new Thread(() => CheckInternetTime());
+            t.Start();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void CheckInternetTime()
+        { 
+            bool isConnected = TGMTonline.IsInternetAvailable();
+
+            DisplayResult(isConnected);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void DisplayResult(bool isConnected)
+        {
+            this.Invoke(new MethodInvoker(delegate
+            {
+                this.TopMost = !isConnected;
+
+                if (isConnected)
+                {
+                    pictureBox1.Image = Properties.Resources.check_mark;
+                    m_soundPlayed = false;
+                    panel1.Visible = false;
+                }
+                else
+                {
+                    this.Show();
+                    this.BringToFront();
+                    this.WindowState = FormWindowState.Normal;
+
+                    pictureBox1.Image = Properties.Resources.check_error;
+
+                    if(!m_soundPlayed)
+                    {
+                        m_soundPlayed = true;
+                        TGMTsound.PlaySoundAsync("FAIL.mp3");
+                    }
+
+                    panel1.Visible = true;
+
+                }
+
+                timerChecktime.Start();
+            }));
+            
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void SendEmailWrongTime()
+        {
+            string api = "/api/email/send";
+            var values = new Dictionary<string, string>
+                {
+                    {"title", "Máy tính " +  Environment.MachineName + " sai ngày tháng"},
+                    {"userReceive", userReceive},
+                    {"purpose", "WrongDatetime"},
+                    {"body", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")},
+                    {"secretKey", secretKey}
+                };
+            TGMTonline.SendPOSTrequest(server + api, values, OnResponse);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            m_isUserClose = true;
+            timerChecktime.Stop();
+            notifyIcon1.Dispose();
+            Application.Exit();
+        }
+    }
+}
