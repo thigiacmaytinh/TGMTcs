501b6bf7a34b208878a7ccf6af9904aa4d4572b7
vohungvi <vohungvi@vohungvi.com>
2025-01-08 16:38:17 +0700

    + add webcam example

---
+++ 3.3_WebcamAforge/WebcamAforge/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WebcamAforge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picWebcam = new System.Windows.Forms.PictureBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.cbCamera = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebcam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbCamera);
+            this.panel1.Controls.Add(this.btn_stop);
+            this.panel1.Controls.Add(this.btn_start);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 77);
+            this.panel1.TabIndex = 0;
+            // 
+            // picWebcam
+            // 
+            this.picWebcam.BackColor = System.Drawing.Color.White;
+            this.picWebcam.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picWebcam.Location = new System.Drawing.Point(0, 77);
+            this.picWebcam.Name = "picWebcam";
+            this.picWebcam.Size = new System.Drawing.Size(800, 373);
+            this.picWebcam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picWebcam.TabIndex = 26;
+            this.picWebcam.TabStop = false;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.Location = new System.Drawing.Point(334, 14);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(88, 40);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_stop.Location = new System.Drawing.Point(452, 14);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(88, 40);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // cbCamera
+            // 
+            this.cbCamera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCamera.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCamera.FormattingEnabled = true;
+            this.cbCamera.Location = new System.Drawing.Point(33, 23);
+            this.cbCamera.Name = "cbCamera";
+            this.cbCamera.Size = new System.Drawing.Size(264, 25);
+            this.cbCamera.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picWebcam);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picWebcam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picWebcam;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.ComboBox cbCamera;
+    }
+}
+
---
+++ 3.3_WebcamAforge/WebcamAforge/Form1.cs
@@ -0,0 +1,153 @@
+﻿//CÔNG TY TNHH GIẢI PHÁP THỊ GIÁC MÁY TÍNH
+//support@viscomsolution.com
+//0939.825.125
+
+
+using AForge.Video.DirectShow;
+using AForge.Video;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace WebcamAforge
+{
+    public partial class Form1 : Form
+    {
+        VideoCaptureDevice m_videoSource;
+        Bitmap m_bmp;
+        double m_aspect = 1.0;
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitCamera();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopWebcam();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void InitCamera()
+        {
+            cbCamera.Items.Clear();
+
+            FilterInfoCollection videosources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videosources.Count == 0)
+            {                
+                return;
+            }
+
+
+            for (int i = 0; i < videosources.Count; i++)
+            {
+                cbCamera.Items.Add(videosources[i].Name);
+            }
+            cbCamera.Enabled = true;
+            if (cbCamera.Items.Count == 1)
+            {
+                cbCamera.SelectedIndex = 0;
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            StartWebcam();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            StopWebcam();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void StartWebcam()
+        {
+            if (cbCamera.Items.Count == 0 || cbCamera.SelectedIndex == -1)
+                return;
+            if (m_videoSource != null)
+            {
+                m_videoSource.Stop();
+            }
+            else
+            {
+                FilterInfoCollection videosources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                m_videoSource = new VideoCaptureDevice(videosources[cbCamera.SelectedIndex].MonikerString);
+                m_videoSource.VideoResolution = selectResolution(m_videoSource); ;
+            }
+
+            m_videoSource.NewFrame += new NewFrameEventHandler(OnCameraFrame);
+            m_videoSource.Start();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        VideoCapabilities selectResolution(VideoCaptureDevice device)
+        {
+            foreach (var cap in device.VideoCapabilities)
+            {
+                if (cap.FrameSize.Height == 1080)
+                {
+                    m_aspect = (double)cap.FrameSize.Width / cap.FrameSize.Height;
+                    return cap;
+                }
+
+                if (cap.FrameSize.Width == 1920)
+                {
+                    m_aspect = (double)cap.FrameSize.Width / cap.FrameSize.Height;
+                    return cap;
+                }
+            }
+
+            VideoCapabilities videoCap = device.VideoCapabilities.Last();
+            m_aspect = (double)videoCap.FrameSize.Width / videoCap.FrameSize.Height;
+            return videoCap;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void StopWebcam()
+        {
+            if (m_videoSource != null)
+            {
+                m_videoSource.Stop();
+            }
+            m_videoSource = null;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void OnCameraFrame(object sender, NewFrameEventArgs eventArgs)
+        { 
+            //m_bmp = (Bitmap)eventArgs.Frame.Clone();
+            picWebcam.Image = new Bitmap(eventArgs.Frame);
+        }
+
+        
+    }
+}
